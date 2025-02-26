4e2e45a34a64b0f68609d5f17f124bf6f6ea1216
leanstepcop <leandrovr19@gmail.com>
2025-02-26 20:57:45 -0300

    feito

---
+++ FixacaoConceitosBasicos1/Model/CalculaAumento.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class CalculaAumento
+{
+    public static double CalculaAument(double sal, double aumet)
+    {
+        return sal + aumet;
+    }
+
+    public static double CalculaDesconto(double valor)
+    {
+        double desconto = valor * 0.2;
+        double newValor = desconto;
+
+        return valor - desconto;
+    }
+
+    public static double CalculaAluguel(int dias, int kmIni, int kmFin)
+    {
+        double pricePerKm = 0.4;
+        double dailyPrice = 80;
+
+        double totalKm = kmIni - kmFin;
+
+        double totalPrice = (dias * dailyPrice) + (pricePerKm * totalKm);
+
+        return totalPrice;
+
+    }
+
+
+}
+//olá
